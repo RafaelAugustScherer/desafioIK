6800c436b1aec7f0f907c027fdf69876f4580263
rafaelaugustscherer <rafaelaugustscherer@gmail.com>
2022-06-07 14:56:59 -0300

    Add: Deploy Workflow

--- api/src/app/database/config/config.ts
+++ api/src/app/database/config/config.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.PS_USERNAME,
-  password: process.env.PS_PASSWORD,
-  database: `${process.env.PS_DATABASE}`,
-  host: process.env.PS_HOST || 'localhost',
-  port: Number(process.env.PS_PORT) || 5432,
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: `${process.env.MYSQL_DATABASE}`,
+  host: process.env.MYSQL_HOST || 'localhost',
+  port: Number(process.env.MYSQL_PORT) || 3306,
   dialect: 'postgres',
   dialectOptions: {
     timezone: '-03:00',
